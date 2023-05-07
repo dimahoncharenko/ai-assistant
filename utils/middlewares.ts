b443e873f8cdbd4ef03630a936f03e3c50a4338b
Dima Honcharenko <d.goncharenko.man@gmail.com>
2023-05-07 11:27:36 +0300

    Adjusted a middleware

--- utils/middlewares.ts
+++ utils/middlewares.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 
 export const checkParams = (params: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
+    console.log("Test probe: ", req.body);
+
     params.forEach((name) => {
       if (!req.body[name]) throw new Error(`Параметр: ${name} обов'язковий!`);
     });
